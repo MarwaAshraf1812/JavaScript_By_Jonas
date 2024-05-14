06351bf1882b23aaf0f74ba2f7e36cadf40b5eb2
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-14 14:55:29 +0300

    first

---
+++ section_2/challenge_1.js
@@ -0,0 +1,16 @@
+/* Write your code below. Good luck! 🙂 */
+
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark);
+console.log(BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
