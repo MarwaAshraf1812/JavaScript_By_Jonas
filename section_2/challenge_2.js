0b8d52b9bb46bc3c213878fb72b52ce27da88ff8
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-14 14:55:43 +0300

    second🙂

---
+++ section_2/challenge_2.js
@@ -0,0 +1,19 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+let markHigherBMI = BMIMark > BMIJohn;
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than john's (${BMIJohn})!`);
+} else  {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+
+
