708df7f7b757f331cac348f34bf7f61502fbe806
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-14 14:55:59 +0300

    third 🙂

---
+++ section_2/challenge_3.js
@@ -0,0 +1,8 @@
+/* Write your code below. Good luck! 🙂 */
+
+let scoreDolphins = (96 + 108 + 89) / 3;
+let scoreKoalas = (88 + 91 + 110 ) / 3;
+
+if (scoreDolphins > scoreKoalas ) console.log("Dolphins win the trophy");
+else if (scoreDolphins < scoreKoalas ) console.log("Koalas win the trophy");
+else if (scoreKoalas === scoreDolphins) console.log("Both win the trophy")
